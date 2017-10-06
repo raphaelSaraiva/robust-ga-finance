304d0849c27ec81a428926b1ebd9da304cd419e4
raphael.saraiva <raphael.saraiva@aluno.uece.br>
2017-10-05 21:34:07 -0300

    first commit

---
+++ src/jmetal/operators/selection/OperadorTest.java
@@ -0,0 +1,26 @@
+package jmetal.operators.selection;
+
+import java.util.HashMap;
+
+import jmetal.core.Solution;
+import jmetal.core.SolutionSet;
+import jmetal.util.JMException;
+
+public class OperadorTest extends Selection{
+
+	public OperadorTest(HashMap<String, Object> parameters) {
+		super(parameters);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Object execute(Object object) throws JMException {
+		
+	    SolutionSet solutionSet = (SolutionSet)object;
+	    Solution solution1, solution2;
+		
+		
+		return null;
+	}
+
+}
